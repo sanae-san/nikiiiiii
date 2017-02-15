08636602bf50e74f8f9536fe3fd8f333e09027a3
sanae <tatsutthk@gmail.com>
2017-02-15 23:01:47 +0900

    Add files via upload

--- nikiiiii/index.js
+++ nikiiiii/index.js
@@ -33,26 +33,23 @@ var element = document.getElementById("sw_13");var isChecked = element.checked;i
 var element = document.getElementById("sw_12");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_12.value);}
 var element = document.getElementById("sw_11");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_11.value);}
 var element = document.getElementById("sw_10");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_10.value);}
-var element = document.getElementById("sw_9");var isChecked = element.checked;if(isChecked){srcs.push('./img/09_ズボン.png');}
-var element = document.getElementById("sw_8");var isChecked = element.checked;if(isChecked){srcs.push('./img/08_腕輪.png');}
-var element = document.getElementById("sw_7");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_07.value);}
-var element = document.getElementById("sw_6");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_06.value);}
-var element = document.getElementById("sw_5");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_05.value);}
-var element = document.getElementById("sw_4");var isChecked = element.checked;if(isChecked){srcs.push('./img/04_クマ.png');}
-var element = document.getElementById("sw_3");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_03.value);}
-var element = document.getElementById("sw_2");var isChecked = element.checked;if(isChecked){srcs.push('./img/02_髪1.png');}
-var element = document.getElementById("sw_1");var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_01.value);}
+var element = document.getElementById("sw_9"); var isChecked = element.checked;if(isChecked){srcs.push('./img/09_ズボン.png');}
+var element = document.getElementById("sw_8"); var isChecked = element.checked;if(isChecked){srcs.push('./img/08_腕輪.png');}
+var element = document.getElementById("sw_7"); var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_07.value);}
+var element = document.getElementById("sw_6"); var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_06.value);}
+var element = document.getElementById("sw_5"); var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_05.value);}
+var element = document.getElementById("sw_4"); var isChecked = element.checked;if(isChecked){srcs.push('./img/04_クマ.png');}
+var element = document.getElementById("sw_3"); var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_03.value);}
+var element = document.getElementById("sw_2"); var isChecked = element.checked;if(isChecked){srcs.push('./img/02_髪1.png');}
+var element = document.getElementById("sw_1"); var isChecked = element.checked;if(isChecked){srcs.push(document.fm.fig_01.value);}
 
 
-
-
-//ロード？
+//↑で設定した画像ファイルを読み込む
     var images = [];
     for (var i in srcs) {
      		
 		images[i] = new Image();
         images[i].src = srcs[i];
-		
 		
     }
 
@@ -66,8 +63,6 @@ var element = document.getElementById("sw_1");var isChecked = element.checked;if
                 var y = 0;
                 for (var j in images) {
                     context.drawImage(images[j], x, y, 800, 1200);
-                    x += 0;
-                    y += 0;
                 }
             }
             loadedCount++;
@@ -100,6 +95,55 @@ function hoz(){
 var canvas = document.getElementById("canvas");  //canvas要素を取得
   var data = canvas.toDataURL('image/png');
 
-    data.replace('image/png', 'image/octet-stream');
-    window.open(data, 'save');
+
+  var base64 = this.canvas.toDataURL('image/png');
+    // Base64からバイナリへ変換
+  var bin = atob(base64.replace(/^.*,/, ''));
+  var buffer = new Uint8Array(bin.length);
+  for (var i = 0; i < bin.length; i++) {
+      buffer[i] = bin.charCodeAt(i);
+  }
+    // Blobを作成
+  var blob = new Blob([buffer.buffer], {
+      type: 'image/png'
+  });
+
+
+  uploadImageToImgur(blob);
+
 	}
+
+
+
+var uploadImageToImgur = function (blob) {
+  var formData = new FormData();
+  formData.append('image', blob);
+   $.ajax({
+    contentType: false,
+    data: formData,
+    headers: {
+        Authorization: 'Client-ID 82425c572eb53e8',
+    },
+    processData: false,
+    type: 'POST',
+    url: 'https://api.imgur.com/3/image',
+    success: function (result) {
+        var id = result.data.id;
+        //window.location = 'https://imgur.com/gallery/' + id;
+
+        //return id;
+        var file = 'http://i.imgur.com/' + id + '.png';
+
+		twi(file)
+     
+       
+    }
+  });
+};
+
+
+
+function twi(file) {
+    window.open('http://twitter.com/home?status=' + encodeURI(document.title) + '%20' + encodeURI(file) + '+%23' + encodeURI('ミラクル_タツ_ニキ'), '_blank');
+};
+
