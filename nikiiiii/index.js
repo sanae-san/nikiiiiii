ab6cee8c3c1e16243d82e7fa79805d66aa6ccdeb
sanae <tatsutthk@gmail.com>
2017-02-19 03:21:57 +0900

    Add files via upload

--- nikiiiii/index.js
+++ nikiiiii/index.js
@@ -12,6 +12,7 @@ window.onload = function()
 };
 
 function draw() {
+
   /* canvas要素のノードオブジェクト */
   var canvas = document.getElementById("canvas");
   /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
@@ -68,30 +69,19 @@ var element = document.getElementById("sw_1"); var isChecked = element.checked;i
             loadedCount++;
         }, false);
     }
-  
-}
-
-
-function twitText() {
-	var s, url;
-	s = "投稿するテキスト";
-	url = document.location.href;
 	
-	if (s != "") {
-		if (s.length > 140) {
-			//文字数制限
-			alert("テキストが140字を超えています");
-		} else {
-			//投稿画面を開く
-			url = "http://twitter.com/share?url=" + escape(url) + "&text=" + s;
-			window.open(url,"_blank","width=600,height=300");
-		}
-	}
+
 }
 
 
+
+
+
+
+//保存
 function hoz(){
 	
+
 var canvas = document.getElementById("canvas");  //canvas要素を取得
   var data = canvas.toDataURL('image/png');
 
@@ -111,6 +101,7 @@ var canvas = document.getElementById("canvas");  //canvas要素を取得
 
   uploadImageToImgur(blob);
 
+
 	}
 
 
@@ -121,29 +112,62 @@ var uploadImageToImgur = function (blob) {
    $.ajax({
     contentType: false,
     data: formData,
-    headers: {
-        Authorization: 'Client-ID 82425c572eb53e8',
-    },
+    headers: {Authorization: 'Client-ID 82425c572eb53e8',    },
     processData: false,
     type: 'POST',
     url: 'https://api.imgur.com/3/image',
     success: function (result) {
         var id = result.data.id;
         //window.location = 'https://imgur.com/gallery/' + id;
-
         //return id;
-        var file = 'http://i.imgur.com/' + id + '.png';
-
+        var file = ' http://imgur.com/' + id;
+		$.unblockUI();
 		twi(file)
-     
-       
+    },
+    error: function (responseData, textStatus, errorThrown) {
+        alert('GET failed.');
     }
-  });
+
+  });//ajax
 };
 
 
 
 function twi(file) {
-    window.open('http://twitter.com/home?status=' + encodeURI(document.title) + '%20' + encodeURI(file) + '+%23' + encodeURI('ミラクル_タツ_ニキ'), '_blank');
+	//document.title
+    //window.open('http://twitter.com/home?status=' + encodeURI('')  + '%0A%23' + encodeURI('ミラクル_タツ_ニキ') + '%0A%20' + encodeURI(file), '_blank');
+	window.location.href = 'https://twitter.com/intent/tweet?original_referer=' + encodeURI(window.location.href ) +'&ref_src=twsrc%5Etfw&text=' + encodeURI('')  + '%0A%23' + encodeURI('ミラクル_タツ_ニキ') + '%0A%20' + encodeURI(file);
+
 };
 
+
+
+
+
+
+$(function(){
+	//ボタンエンターをクリックしたときの処理
+	$('#btn_ent').click(function(){
+        $.blockUI();
+		 draw();
+		setTimeout(function(){
+	        $.unblockUI();
+		},1000);
+    });
+	
+	//ボタンつぶやくを押したときの処理
+    $('#btn_twi').click(function() {
+
+         $.blockUI();
+		  
+		  hoz();
+		  
+		setTimeout(function(){
+	        $.unblockUI();
+		},1000);     
+
+    }); 
+}); 
+
+
+  
